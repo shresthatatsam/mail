a419b48b6b81851001fcc64cec5c7b05a2edcc94
shresthatatsam <117971671+shresthatatsam@users.noreply.github.com>
2023-12-11 11:01:34 +0545

    Add files via upload
    change

---
+++ HomeController.cs
@@ -0,0 +1,27 @@
+﻿using mail.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace mail.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IEmailSender _emailsender;
+
+        public HomeController(IEmailSender emailSender)
+        {
+            _emailsender = emailSender;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var receiver = "shreya.prajapati@info.com.np";
+            var subject = "test mail abcderf";
+            var message = "hello shreya";
+            await _emailsender.SendEmailAsync(receiver, subject, message);
+            return View();
+        }
+
+    
+    }
+}
