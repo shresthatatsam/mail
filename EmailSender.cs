8173a9c62e0441d5f3342cbe983c0d93ca47cc50
shresthatatsam <117971671+shresthatatsam@users.noreply.github.com>
2023-12-11 11:01:06 +0545

    Add files via upload
    second

---
+++ EmailSender.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Net.Mail;
+
+namespace mail
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            var mail = "tatsam.shrestha@outlook.com";
+            var password = "usepassword";
+
+            var client = new SmtpClient("smtp-mail.outlook.com", 587)
+            {
+                EnableSsl = true,
+                Credentials = new NetworkCredential(mail, password)
+            };
+
+            return client.SendMailAsync(
+                new MailMessage(
+                    from: mail,
+                    to: email,
+                    subject, message));
+         
+        }
+    }
+}
---
+++ IEmailSender.cs
@@ -0,0 +1,7 @@
+﻿namespace mail
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email , string subject ,string message);
+    }
+}
